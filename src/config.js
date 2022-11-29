7b53cc8b2fa53a51cb857758b2b995cc4dac8efd
JenHLee <hj.jennie.lee@gmail.com>
2022-11-28 22:22:51 -0700

    added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : "https://jennieblog.herokuapp.com/api/"
+    baseURL : "https://jenlog.herokuapp.com/api/"
+})
+
+export const proxyLocal = axios.create({
+    baseURL : "https://localHost:5000/api/"
 })
