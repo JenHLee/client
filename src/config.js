3670c7c2421da4f333b1daf6dcaad1be634b8e9f
JenHLee <hj.jennie.lee@gmail.com>
2022-11-28 21:35:37 -0700

    added / in config .js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : "https://jennieblog.herokuapp.com/api"
+    baseURL : "https://jennieblog.herokuapp.com/api/"
 })
